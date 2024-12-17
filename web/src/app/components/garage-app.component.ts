144c899a8d1ffcf5a83a8437ef801809e0e48e88
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-16 22:54:56 -0300

    feat: impl method findAll in garage-app component

--- web/src/app/components/garage-app.component.ts
+++ web/src/app/components/garage-app.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Vehicle } from '../models/vehicle';
+import { VehicleService } from '../services/vehicle.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-garage-app',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './garage-app.component.html',
 })
-export class GarageAppComponent {
+export class GarageAppComponent implements OnInit {
   title: string = 'Listado de Vehiculos';
+
+  vehicles: Vehicle[] = [];
+
+  constructor(private vehicleService: VehicleService) {}
+
+  ngOnInit(): void {
+    this.vehicleService
+      .findAll()
+      .subscribe((vehicle) => (this.vehicles = vehicle));
+  }
 }
