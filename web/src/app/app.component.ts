ebd143e92bb085afe73c7ed1a37fdb8d24f8154a
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-16 22:27:51 -0300

    feat: create principal garage-app component

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { GarageAppComponent } from './components/garage-app.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [GarageAppComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'web';
---
+++ web/src/app/components/garage-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-garage-app',
+  standalone: true,
+  imports: [],
+  templateUrl: './garage-app.component.html',
+})
+export class GarageAppComponent {
+  title: string = 'Listado de Vehiculos';
+}
