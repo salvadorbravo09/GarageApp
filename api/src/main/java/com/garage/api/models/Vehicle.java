8d0fa5ac842c7ebb2262aa0c6f6b64971157caf0
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-23 22:35:23 -0300

    feat: create vehicle entity

---
+++ api/src/main/java/com/garage/api/models/Vehicle.java
@@ -0,0 +1,26 @@
+package com.garage.api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "vehicles")
+public class Vehicle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String brand;
+    private String model;
+    private Integer year;
+    private String color;
+    private String transmision;
+}
