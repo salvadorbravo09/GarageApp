7870b30a25a2d88ecb9048790398ae0b6daf6acb
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-23 22:36:11 -0300

    feat: create vehicle repository with annotation

---
+++ api/src/main/java/com/garage/api/repositories/VehicleRepository.java
@@ -0,0 +1,9 @@
+package com.garage.api.repositories;
+
+import com.garage.api.models.Vehicle;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends CrudRepository<Vehicle, Long> {
+}
