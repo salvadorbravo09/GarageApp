c48e2060b83b8233ab8733e0d484cba0ae648456
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-25 20:51:45 -0300

    feat: implementing DELETE method and refactor code

--- api/src/main/java/com/garage/api/controllers/VehicleController.java
+++ api/src/main/java/com/garage/api/controllers/VehicleController.java
@@ -26,9 +26,9 @@ public class VehicleController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Vehicle> getVehicle(@PathVariable Long id) {
-        Optional<Vehicle> optionalVehicle = vehicleService.findById(id);
-        if (optionalVehicle.isPresent()) {
-            return ResponseEntity.status(HttpStatus.OK).body(optionalVehicle.orElseThrow());
+        Optional<Vehicle> existsVehicle = vehicleService.findById(id);
+        if (existsVehicle.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(existsVehicle.orElseThrow());
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
@@ -50,6 +50,17 @@ public class VehicleController {
             savedVehicle.setColor(vehicle.getColor());
             savedVehicle.setTransmision(vehicle.getTransmision());
             return ResponseEntity.status(HttpStatus.OK).body(vehicleService.save(savedVehicle));
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteVehicle(@PathVariable Long id) {
+        Optional<Vehicle> existsVehicle = vehicleService.findById(id);
+
+        if (existsVehicle.isPresent()) {
+            vehicleService.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
         }
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
