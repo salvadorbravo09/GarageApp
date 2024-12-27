d4b3b106db900014a1e82eb53b0b3fd189f46f65
Salvador Bravo Carrillo <sa.bravo0901@gmail.com>
2024-12-26 22:09:54 -0300

    refactor: delete lombok and add manual annotation

--- api/src/main/java/com/garage/api/models/Vehicle.java
+++ api/src/main/java/com/garage/api/models/Vehicle.java
@@ -7,10 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "vehicles")
 public class Vehicle {
 
@@ -23,4 +19,64 @@ public class Vehicle {
     private Integer year;
     private String color;
     private String transmision;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(Long id, String brand, String model, Integer year, String color, String transmision) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.transmision = transmision;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
 }
